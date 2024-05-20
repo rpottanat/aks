1860beed7356a14247339a7fa05bb1cb3ae100b4
rpott <rpott@VADANYA>
2024-05-20 11:43:17 +1000

    commit all

---
+++ Shopping/Shopping.Client/Data/ProductContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Shopping.Client.Models;
+
+namespace Shopping.Client.Data
+{
+    public static class ProductContext
+    {
+
+        public static readonly List<Product> Products = new List<Product>
+        {
+            new Product()
+            {
+                Name = "Test",
+                Description = "Test",
+                ImageFile="image1",
+                Price = 0.25M,
+                Category = "Test",
+
+            },
+            new Product()
+            {
+                Name = "Test",
+                Description = "Test",
+                ImageFile="image1",
+                Price = 0.25M,
+                Category = "Test",
+
+            },
+            new Product()
+            {
+                Name = "Test",
+                Description = "Test",
+                ImageFile="image1",
+                Price = 0.25M,
+                Category = "Test",
+
+            }
+        };
+    }
+}
---
+++ Shopping/Shopping.Client/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace Shopping.Client.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        public required string Name { get; set; }
+        public required string Category { get; set; }
+        public required string Description { get; set; }
+        public required string ImageFile { get; set; }
+        public decimal Price { get; set; }      
+
+    }
+}
